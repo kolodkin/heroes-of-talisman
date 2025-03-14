94d7e3250abb69df7468b7969c1a4425b7bc0ddb
kolodkin <mark.kolodkin@gmail.com>
2025-03-14 22:27:31 +0200

    project backbone

--- server/main.py
+++ server/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI, WebSocket
+from fastapi.responses import PlainTextResponse
 import uvicorn
-import aioredis
+
+# import aioredis
 
 app = FastAPI()
 
 # Initialize Redis client
-redis_client = aioredis.from_url("redis://localhost")
+# redis_client = aioredis.from_url("redis://localhost")
+
+
+@app.get("/")
+async def read_root():
+    return PlainTextResponse("Welcome to 'Heroes of Talisman' game server!")
+
+
+@app.get("/health")
+async def read_root():
+    return PlainTextResponse("Server is up")
+
 
 @app.websocket("/ws/{game_id}")
 async def websocket_endpoint(websocket: WebSocket, game_id: str):
@@ -13,9 +26,10 @@ async def websocket_endpoint(websocket: WebSocket, game_id: str):
     while True:
         data = await websocket.receive_text()
         # Process the received data and update the game state in Redis
-        await redis_client.set(game_id, data)
+        # await redis_client.set(game_id, data)
         # Broadcast the updated game state to all connected clients
         await websocket.send_text(data)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
