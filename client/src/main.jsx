33bde4b2bb3329a052fb78d674f0c72b33df8ed6
kolodkin <mark.kolodkin@gmail.com>
2025-03-15 01:01:31 +0200

    add redis

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import HomePage from './components/HomePage.jsx';
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>,
+    <StrictMode>
+        <HomePage />
+        <ToastContainer />
+    </StrictMode>,
 );
