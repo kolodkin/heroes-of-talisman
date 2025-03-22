c4c56b1afeb764ee02367a62ef0c6094dcb0f58b
kolodkin <mark.kolodkin@gmail.com>
2025-03-22 10:24:06 +0200

    game engine update

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 import logging
+import asyncio
 import json
 from http import HTTPStatus
 
@@ -83,44 +84,90 @@ async def check_game_name(game: Game):
     return {"isUnique": is_unique}
 
 
-# async def actions_loop():
-#     while True:
+class RedisMeta:
+    def __init__(self, gamename: str, username: str = None):
+        self.gamename = gamename
+        self.username = username
+
+    @property
+    def channel(self):
+        return f"game:{self.gamename}-event"
+
+    @property
+    def key(self):
+        return f"game:{self.gamename}"
 
 
-@app.websocket("/ws/{game_name}/{username}")
-async def websocket_endpoint(websocket: WebSocket, game_name: str, username: str):
-    if not await redis_client.exists(f"game:{game_name}"):
-        raise HTTPException(status_code=404, detail="Game not found")
+async def from_redis(redis: Redis, redis_meta: RedisMeta) -> "GameEngine":
+    game = await redis.get(redis_meta.key)
+    if game is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Game not found")
+
+    game = json.loads(game)
+    if redis_meta.username is None:
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="missing username")
+
+    return GameEngine(redis_meta.gamename, redis_meta.username, game)
+
+
+async def game_update_loop(websocket: WebSocket, redis_meta: RedisMeta):
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(redis_meta.channel)
+    async for message in pubsub.listen():
+        if message["type"] == "message":
+            data = json.loads(message["data"])
+            if data["event"] == "game_update":
+                game_engine = await from_redis(redis_client, redis_meta)
+                await websocket.send_json(game_engine.game)
+            else:
+                logger.warning(f"Unknown event: {data}")
+
+
+async def actions_loop(websocket: WebSocket, redis: Redis, redis_meta: RedisMeta):
+    while True:
+        action = await websocket.receive_text()
+        action = json.loads(action)
+        logger.info(f"Received action: {action}")
+
+        try:
+            game_engine = await from_redis(redis, redis_meta)
+            await game_engine.action(action)
+        except GameException as e:
+            raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=f"Invalid action: {e}")
+        except Exception as e:
+            raise HTTPException(
+                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                detail=f"Unexpeced Exception: {e.__class__.__name__}:{e}",
+            )
+
+        logger.info(f"Sending game state to user '{game_engine.username}'")
+        await redis_client.set(redis_meta.key, game_engine.dumps())
+        await redis_client.publish(redis_meta.channel, json.dumps(dict(event="game_update")))
+
+
+@app.websocket("/ws/{gamename}/{username}")
+async def ws_game_endpoint(websocket: WebSocket, gamename: str, username: str):
+    redis_meta = RedisMeta(gamename, username)
+
+    if not await redis_client.exists(redis_meta.key):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Game not found")
 
     await websocket.accept()
 
-    logger.info(f"Connection to game {game_name} established for user {username}")
+    logger.info(f"Connection to game {gamename} established for user {username}")
     try:
-        game_engine = await GameEngine.from_redis(game_name, username, redis_client)
-        while True:
-            action = await websocket.receive_text()
-            action = json.loads(action)
-            logger.info(f"Received action: {action}")
-
-            try:
-                await game_engine.action(action)
-            except GameException as e:
-                raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=f"Invalid action: {e}")
-            except Exception as e:
-                raise HTTPException(
-                    status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-                    detail=f"Unexpeced Exception: {e.__class__.__name__}:{e}",
-                )
-
-            logger.info(f"Sending game state to user {username}")
-            await redis_client.set(f"game:{game_name}", json.dumps(game_engine.game))
-            await websocket.send_json(game_engine.game)
+        await asyncio.gather(
+            game_update_loop(websocket, redis_meta),
+            actions_loop(websocket, redis_client, redis_meta),
+        )
     except WebSocketDisconnect:
+        game_engine = await from_redis(redis_client, redis_meta)
         game_engine.disconnect(username)
-        logger.info(f"Client {username} disconnected from game {game_name}")
+        await redis_client.set(redis_meta.key, game_engine.dumps())
+        logger.info(f"Client '{username}' disconnected from game '{gamename}'")
     finally:
         # Perform any cleanup actions here
-        logger.info(f"Connection to game {game_name} closed for user {username}")
+        logger.info(f"Connection to game '{gamename}' closed for user '{username}'")
 
 
 app.include_router(router, prefix="/api/games", tags=["games"])
