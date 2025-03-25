3f23bdbe4340d4c35a4b7d50d26dd68b4ef47ab1
kolodkin <mark.kolodkin@gmail.com>
2025-03-25 07:17:47 +0200

    refactor Game component and improve reconnect logic; update stage names in language file

--- client/src/components/he.js
+++ client/src/components/he.js
@@ -1,6 +1,12 @@
 const lang = {
     playing: 'משחק',
     waiting_his_turn: 'ממתין לתורו',
+    stageNames: {
+        'character_select': 'בחירת דמות',
+        'card_draw': 'שליפת קלף',
+        'use_skill': 'שימוש ביכולת',
+        'battle': 'קרב',
+    },
     stageTitleNames: {
         'character_select': 'בחר דמות',
         'card_draw': 'שלוף קלף',
@@ -11,7 +17,13 @@ const lang = {
         'knight': 'אביר',
         'archer': 'קשת',
         'mage': 'קוסם',
-    }
+    },
+    action_board: {
+        wait_your_turn: 'המתן לתורך',
+    },
+    character_select: {
+        submit: 'שלח',
+    },
 }
 
 export default lang
