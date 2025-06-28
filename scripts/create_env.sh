0426e6001ab7900c92197c60e4632d2a277c1535
mark <mark@tonicsecurity@gmail.com>
2025-06-28 16:51:52 +0300

    remove all ui and backend

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -4,7 +4,15 @@ set -e
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 ROOT_DIR=$SCRIPT_DIR/..
 
+if [ -f $ROOT_DIR/.env.tmp ]; then
+    set +o allexport
+    source $ROOT_DIR/.env.tmp
+    set -o allexport
+fi
+
+
 cd $ROOT_DIR
 echo MY_UID=$(id -u) > .env
 echo MY_GID=$(id -g) >> .env
 echo MY_NODE=$(node --version | sed 's/^v//') >> .env
+echo POSTGRES_PORT=${POSTGRES_PORT:-5432} >> .env
--- server/env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = os.getenv("REDIS_PORT", 6379)
