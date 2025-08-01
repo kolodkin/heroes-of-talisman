9350c14ef04858b6f7427f4fe66fb0f577c34ec3
mark kolodkin <mark@tonicsecurity.com>
2025-08-01 12:51:50 +0000

    test(e2e): capture screenshot and attach

--- e2e/home.spec.cjs
+++ e2e/home.spec.cjs
@@ -3,4 +3,6 @@ const { test, expect } = require('@playwright/test');
 test('home page loads', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByRole('heading', { name: /welcome to heroes of talisman/i })).toBeVisible();
+  const screenshot = await page.screenshot();
+  await test.info().attach('home-page', { body: screenshot, contentType: 'image/png' });
 });
