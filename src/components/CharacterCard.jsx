a064d8f195620c1d5bc56cbf4233aa7a0171240f
mark <mark@tonicsecurity@gmail.com>
2025-10-02 12:51:03 +0300

    great

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -14,19 +14,28 @@ const CharacterCard = ({ name, character, isSelected, onClick }) => {
       className={className({ [commonStyles.selected]: isSelected }, commonStyles.gamebtn, styles.character, "text-2xl")}
       onClick={onClick}
     >
-      <img src={`/images/${name}.png`} alt={name} style={{ minWidth: "200px", width: "200px", height: "200px" }} />
-      <p className="align-text-center w-full">
+      <img
+        src={`/images/${name}.png`}
+        alt={name}
+        style={{
+          minWidth: "120px",
+          width: "120px",
+          height: "120px",
+          borderRadius: "6px",
+        }}
+      />
+      <p className="w-full text-center font-bold" style={{ fontSize: "0.9rem" }}>
         {nameStr} דרגה {character.level}
       </p>
-      <div className="flex items-center space-x-1">
+      <div className="flex items-center gap-1">
         {[...Array(character.dice).keys()].map((i) => (
-          <DiceIcon color="white" fill="black" size={"20px"} key={i} />
+          <DiceIcon color="white" fill="black" size={"16px"} key={i} />
         ))}
-        <span>{signStr(character.attack)}</span>
+        {character.attack && <span className="font-bold text-sm">{signStr(character.attack)}</span>}
       </div>
-      <div className="flex items-center">
-        <HeartIcon color="red" size={"20px"} />
-        <span>
+      <div className="flex items-center gap-1">
+        <HeartIcon color="red" size={"16px"} />
+        <span className="font-bold text-sm">
           [{character.health}/{character.max_health}]
         </span>
       </div>
